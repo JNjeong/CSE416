95f78b7a9b2f234c1b4731d7f998521047d45905
JNjeong <wnsghd0902@gmail.com>
2024-05-18 02:20:41 +0900

    auto
    auto

---
+++ fresh/server/drivedownload_professors.py
@@ -0,0 +1,66 @@
+
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import io
+from googleapiclient.http import MediaIoBaseDownload
+
+#pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+CREDENTIAL_DIR = './credentials'
+CREDENTIAL_FILENAME = 'drive-python-quickstart.json'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'fresher'
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+
+FILE_ID = '1anakO6VzaLjQ0Yssmq-_wh3x8jDRyOzG5ROKyuJKz64'
+EXCEL = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+EXPORTED_FILE_NAME = 'professors.xlsx'
+
+def get_credentials():
+    credential_dir = CREDENTIAL_DIR
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, CREDENTIAL_FILENAME)
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+    
+    request = service.files().export_media(fileId=FILE_ID, mimeType=EXCEL)
+    fh = io.FileIO(EXPORTED_FILE_NAME, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print('Download %d%%.' % int(status.progress() * 100))
+
+
+if __name__ == '__main__':
+    main()
---
+++ fresh/server/driveupload.py
@@ -0,0 +1,43 @@
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+if __name__ == "__main__":
+    try :
+        import argparse
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    except ImportError:
+        flags = None
+
+    SCOPES = 'https://www.googleapis.com/auth/drive.file'
+    store = file.Storage('storage.json')
+    creds = store.get()
+
+    if not creds or creds.invalid:
+        print("make new storage data file ")
+        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+        creds = tools.run_flow(flow, store, flags) \
+                if flags else tools.run(flow, store)
+
+    DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
+
+    # qgpt 키워드 가능
+    FILES = (
+        ('course_semester.xlsx'),
+        ('courses.xlsx'),
+        ('courses2.xlsx'),
+        ('professors.xlsx'),
+        ('qa.xlsx'),
+        ('qgpt.xlsx')
+    )
+
+    for file_title in FILES :
+        file_name = file_title
+        metadata = {'name': file_name,
+                    'mimeType': None
+                    }
+
+        res = DRIVE.files().create(body=metadata, media_body=file_name).execute()
+        if res:
+            print('Uploaded "%s" (%s)' % (file_name, res['mimeType']))
+            
